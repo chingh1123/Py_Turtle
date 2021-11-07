891914eac85e902892ed4f34d1acdd307547e0f5
ChinGH <86087456+chinghytb@users.noreply.github.com>
2021-11-07 20:58:35 +0800

    Create 6 sides shape.py

---
+++ 6 sides shape.py
@@ -0,0 +1,34 @@
+import turtle
+
+turtle.bgcolor('green')
+gh = turtle.Turtle()
+gh.color('yellow', 'green')
+gh.shape('turtle')
+gh.pensize(40)
+gh.speed(10)
+
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+#-----------------------------
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+#-----------------------------
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+#-----------------------------
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+gh.left(45)
+gh.forward(100)
+
+turtle.done()
